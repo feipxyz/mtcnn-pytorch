11251ec0a10122eb9b88a56dedee2aba5c972bff
feipxyz <feipxyz@163.com>
2019-02-27 12:24:41 +0800

    修改程序中的路径

--- mtcnn/data_preprocess/assemble_pnet_imglist.py
+++ mtcnn/data_preprocess/assemble_pnet_imglist.py
@@ -3,11 +3,14 @@ import sys
 sys.path.append(os.getcwd())
 import mtcnn.data_preprocess.assemble as assemble
 
-pnet_postive_file = './anno_store/pos_12.txt'
-pnet_part_file = './anno_store/part_12.txt'
-pnet_neg_file = './anno_store/neg_12.txt'
-pnet_landmark_file = './anno_store/landmark_12.txt'
-imglist_filename = './anno_store/imglist_anno_12.txt'
+# 项目基目录
+base_path = os.path.join(os.getcwd(), '../..')
+
+pnet_postive_file = os.path.join(base_path, './anno_store/pos_12.txt')
+pnet_part_file = os.path.join(base_path, './anno_store/part_12.txt')
+pnet_neg_file = os.path.join(base_path, './anno_store/neg_12.txt')
+pnet_landmark_file = os.path.join(base_path, './anno_store/landmark_12.txt')
+imglist_filename = os.path.join(base_path, './anno_store/imglist_anno_12.txt')
 
 if __name__ == '__main__':
 
